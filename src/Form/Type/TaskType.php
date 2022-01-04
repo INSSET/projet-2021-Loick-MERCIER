ce40ffa7e402cc13e05622e94efad0e1a522efa9
locki <loick.mercier7@gmail.com>
2022-01-04 14:32:11 +0100

    Feature : Added form to add task to a customer

---
+++ src/Form/Type/TaskType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('text', TextareaType::class)
+            ->add('date', DateType::class, [
+                'widget' => 'choice',
+                'input'  => 'datetime_immutable'
+            ])
+            ->add('save', SubmitType::class)
+        ;
+    }
+}
